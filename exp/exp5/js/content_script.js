596b627ef1459a035a2016e7ef4de4a57bae2951
zasqw2222 <zasqw2222@126.com>
2016-02-02 08:30:43 +0800

    添加 原来的一个项目

---
+++ exp/exp5/js/content_script.js
@@ -0,0 +1,5 @@
+// var header = $('#header');
+// alert($)
+// header.click(function(){
+// 	alert($(this).attr('id'))
+// })
---
+++ exp/exp5/js/popup.js
@@ -0,0 +1,15 @@
+'use strict';
+// chrome.cookies.getAll({}, function(cookies){
+// 	console.log(cookies)
+// })
+// $(function(){
+// 	$.get('http://www.iminer.com/api/getAllrecru',function(data){
+// 		alert(JSON.stringify(data))
+// 	});
+// })
+// alert($)
+
+
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   alert(1)
+// });
